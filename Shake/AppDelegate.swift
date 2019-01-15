3c900e85946625ef4bdaa73131662efcd1fdbb53
Dave Allie <dave@tanda.co>
2019-01-15 19:19:53 +1000

    Initial Commit

---
+++ Shake/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Shake
+//
+//  Created by David Allie on 15/1/19.
+//  Copyright © 2019 Dave Allie. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Shake/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Shake
+//
+//  Created by David Allie on 15/1/19.
+//  Copyright © 2019 Dave Allie. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
